07e152764ca59e446b36e075361d9789c78c9b46
Wendy Moyle <wendymoyle@Wendys-MacBook-Pro.local>
2019-10-12 19:28:12 -0600

    body

--- lesson 4/js/preston.js
+++ lesson 4/js/preston.js
@@ -2,3 +2,11 @@ function toggleMenu(){
 		console.log(document.getElementById("primarynav").classList);
 		document.getElementById("primarynav").classList.toggle("hide");
 }
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth()+1).padStart(2, '0'); //January is 0
+var yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+document.write(today);
