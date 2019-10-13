99228accf358e36fef219072be46da9930b0e70b
Wendy Moyle <wendymoyle@Wendys-MacBook-Pro.local>
2019-10-12 23:05:00 -0600

    date

--- lesson 4/js/preston.js
+++ lesson 4/js/preston.js
@@ -29,15 +29,8 @@ let months = [
 let d = new Date();
 let dayName = daynames[d.getDay()];
 let monthName = months[d.getMonth()];
-let fulldate = dayName + "," + monthName + " " + d.getDate() + " , " + d.getFullYear();
+let fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " + d.getFullYear();
 
 document.getElementById("currentdate").textContent = fulldate;
 
 
-/*var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth()+1).padStart(2, '0'); //January is 0
-var yyyy = today.getFullYear();
-
-today = mm + '/' + dd + '/' + yyyy;
-document.write(today);*/
