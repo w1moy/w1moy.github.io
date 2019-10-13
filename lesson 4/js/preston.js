151fb5f84469ee573d81dd7974f651f557af7138
Wendy Moyle <wendymoyle@Wendys-MacBook-Pro.local>
2019-10-12 22:42:14 -0600

    current date

--- lesson 4/js/preston.js
+++ lesson 4/js/preston.js
@@ -1,11 +1,20 @@
-function toggleMenu(){
-		document.getElementById("primarynav").classList.toggle("hide");
+function toggleMenu() {
+	document.getElementById("primarynav").classList.toggle("hide");
 }
 
-var today = new Date();
+/*var today = new Date();
 var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth()+1).padStart(2, '0'); //January is 0
 var yyyy = today.getFullYear();
 
 today = mm + '/' + dd + '/' + yyyy;
-document.write(today);
+document.write(today);*/
+
+let options = {
+	weekday: "long",
+	day: "numeric",
+	month: "long",
+	year: "numeric"
+};
+
+document.getElementById("currentdate").textContent=new Date().toLocaleDateString("en-US", options); 
