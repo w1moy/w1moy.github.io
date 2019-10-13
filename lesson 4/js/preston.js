4bab2e9100e52356c1e08ef6fb33b7f12da1556f
Wendy Moyle <wendymoyle@Wendys-MacBook-Pro.local>
2019-10-12 23:00:11 -0600

    date

--- lesson 4/js/preston.js
+++ lesson 4/js/preston.js
@@ -2,6 +2,38 @@ function toggleMenu() {
 	document.getElementById("primarynav").classList.toggle("hide");
 }
 
+let daynames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+];
+let months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December",
+];
+
+let d = new Date();
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let fulldate = dayName + "," + monthName + " " + d.getDate() + " , " + d.getFullYear();
+
+document.getElementById("currentdate").textContent = fulldate;
+
+
 /*var today = new Date();
 var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth()+1).padStart(2, '0'); //January is 0
@@ -9,12 +41,3 @@ var yyyy = today.getFullYear();
 
 today = mm + '/' + dd + '/' + yyyy;
 document.write(today);*/
-
-let options = {
-	weekday: "long",
-	day: "numeric",
-	month: "long",
-	year: "numeric"
-};
-
-document.getElementById("currentdate").textContent=new Date().toLocaleDateString("en-US", options); 
