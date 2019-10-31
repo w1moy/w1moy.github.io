2b6825adf108c5c4ef28098a9ef926535d12f422
Wendy Moyle <wendymoyle@Wendys-MacBook-Pro.local>
2019-10-30 23:41:37 -0600

    html

---
+++ lesson 7/css/lazy.css
@@ -0,0 +1,7 @@
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
