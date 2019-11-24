0514d0906717b3e8715c3ac6e36d82839c25922f
Wendy Moyle <wendymoyle@Wendys-MacBook-Pro.local>
2019-11-23 23:53:02 -0700

    html

---
+++ lesson-10/js/weatherapi.js
@@ -0,0 +1,14 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=7494dab38cd1d431535219210538b2c4';
+
+fetch(apiURL)
+.then((response) => response.json())
+.then((jsObject) =>{
+    console.log(jsObject);
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+    const desc = jsObject.weather[0].description;  // note how we reference the weather array
+    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+    document.getElementById('icon').setAttribute('alt', desc);
+});
+
