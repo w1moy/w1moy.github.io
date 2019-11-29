bafd65a2f54fc1239dbca422fe70ce63a21a3fb4
w1moy.github.io <wendymoyle@gmail.com>
2019-11-29 11:08:54 -0700

    html

---
+++ lesson-11/js/main.js
@@ -0,0 +1,40 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+	.then(function (response) {
+		return response.json();
+	})
+
+	.then(function(jsonObject) {
+		const towns = jsonObject['towns'];
+		for (let i = [0]; i<towns.length; i++) 
+		if (towns[i].name === "Fish Haven" || towns[i].name === "Preston" || towns[i].name === "Soda Springs"){
+
+			let town = document.createElement('section');
+			let h2 = document.createElement('h2');
+			let motto = document.createElement('h3');
+			let rain = document.createElement('p');
+			let pop = document.createElement('p');
+			let found = document.createElement('p');
+			let img = document.createElement('img');
+
+			h2.textContent = towns[i].name;
+			motto.textContent = towns[i].motto;
+			found.textContent = "Year Founded:" + " " + towns[i].yearFounded;
+			pop.textContent = "Population:" + " " + towns[i].currentPopulation;
+			rain.textContent = "Average Rainfall:" + " " + towns[i].averageRainfall;
+			img.setAttribute('src',  'images/'+towns[i].photo); 
+			img.setAttribute('alt', towns[i].name + " " + 'town photo');
+
+
+			town.appendChild(h2);
+			town.appendChild(motto);
+			town.appendChild(found);
+			town.appendChild(pop);
+			town.appendChild(rain);
+			town.appendChild(img);
+
+			document.querySelector('div.town').appendChild(town);
+
+		}
+	})
