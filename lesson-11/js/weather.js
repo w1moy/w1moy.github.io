6cb79095d41bad7ad2c68e61b7cd4ed0c6309a5b
w1moy.github.io <wendymoyle@gmail.com>
2019-12-02 20:47:38 -0700

    html

--- lesson-11/js/weather.js
+++ lesson-11/js/weather.js
@@ -1,18 +1,31 @@
 const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=7494dab38cd1d431535219210538b2c4';
 
 fetch(apiURL)
-.then((response) => response.json())
-.then((jsObject) => {
-		console.log(jsObject);
-
+	.then((response) => response.json())
+	.then((jsObject) => {
+		//console.log(jsObject);
+		var temp = Math.round(jsObject.main.temp);
+		var wspeed = jsObject.wind.speed;
+		var wchill = windChill(temp, wspeed);
 		const desc = jsObject.weather[0].description;
-		document.getElementById('description').textContent=desc;
+		document.getElementById('description').textContent = desc;
+		document.getElementById('wind-speed').textContent = jsObject.wind.speed;
+		document.getElementById('current-temp').textContent = jsObject.main.temp;
+		document.getElementById('humidity').textContent = jsObject.main.humidity;
+		document.getElementById('wind-Chill').textContent = wchill;
 
-		const windspeed = jsObject.wind.speed;
-		document.getElementById('wind-speed').textContent=windspeed;
+
+
+		/*function windChill(tempF, speed) {
 	
-		document.getElementById('current-temp').textContent=jsObject.main.temp;
-		
-		document.getElementById('humidity').textContent=jsObject.main.humidity;
-		  
-});
+			if (tempF > 50 || speed <= 3) {
+				var s = Math.pow(speed, .16);
+				var wchill = 35.74 + (0.6215 * tempF) - (35.75 * s) + (.4275 * tempF * s);
+				return wchill;
+			} else {
+				return "N/A";
+			}
+		}*/
+
+
+	});
--- lesson-11/js/windchill.js
+++ lesson-11/js/windchill.js
@@ -1,10 +1,10 @@
-var tempF=parseFloat(document.getElementById ('current-temp').textContent);
-var Speed=parseFloat(document.getElementById ('wind-speed').textContent);
-var calculateNums=windChill(temp, speed);
-document.getElementById('chill').textContent=calculateNums;
-
-function windChill(tempF, speed){
-    var s = Math.pow(speed, .16);
-   var factor = 35.74 + (0.6215*tempF) - (35.75*s) + (.4275*tempF*s);
-   return factor;
-}
+function windChill(temp, wspeed) {
+		if (temp > 50 || wspeed <= 3) {
+			var s = Math.pow(wspeed, .16);
+			var f  = 35.74 + (0.6215 * temp) - (35.75 * s) + (.4275 * temp * s);
+			var wchill=Math.round(f*10)/10
+			return wchill;
+		} else {
+			return "N/A";
+		}
+	}
