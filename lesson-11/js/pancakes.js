037f70dcdfb512889ee9621d532ceaea0be81d06
w1moy.github.io <wendymoyle@gmail.com>
2019-12-07 01:37:22 -0700

    css

--- lesson-11/js/pancakes.js
+++ lesson-11/js/pancakes.js
@@ -1,5 +1,7 @@
 var date = new Date();
 var day = date.getDay();
 if (day == 5){
-    document.getElementById("prestonPancakes").style.display = "block";
-};
+    var ban = "Saturday = Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pavilion."
+    document.getElementById("prestonPancakes").innerHTML = ban;}
+    else {document.getElementById("prestonPancakes").style.display = "none";
+}
