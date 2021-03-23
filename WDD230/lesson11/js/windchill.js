0456acc0682c3a25a08a1a011c1fa9eae445137c
w1moy <wendymoyle@gmail.com>
2021-03-22 23:17:26 -0600

    js

--- WDD230/lesson11/js/windchill.js
+++ WDD230/lesson11/js/windchill.js
@@ -1,13 +1,11 @@
-function doInputOutput () {
+
 var tempF=parseFloat(document.getElementById ("temp").value);
-console.log(tempF)
 var wSpeed=parseFloat(document.getElementById ('speed').value);
 var calculateNums=windChill(temp, speed);
-document.getElementById('chill').textContent=calculateNums;}
+document.getElementById('chill').textContent=calculateNums;
 
 function windChill(tempF, wSpeed){
     var s = Math.pow(wSpeed, .16);
-    console.log(s)
    var factor = 35.74 + (0.6215*tempF) - (35.75*s) + (.4275*tempF*s);
    return factor;
 }
